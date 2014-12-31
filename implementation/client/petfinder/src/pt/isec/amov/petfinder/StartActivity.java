0704ea860a5fe97dde17c3bbd89b16f381754586
Marcos Góis <marcos.avbg@gmail.com>
2014-12-31 03:10:27 +0000

    Added an usage example for the AuthenticateUserTask class

--- implementation/client/petfinder/src/pt/isec/amov/petfinder/StartActivity.java
+++ implementation/client/petfinder/src/pt/isec/amov/petfinder/StartActivity.java
@@ -2,6 +2,7 @@ package pt.isec.amov.petfinder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import pt.isec.amov.petfinder.rest.AuthenticateUserTask;
 
 public class StartActivity extends Activity {
     /**
@@ -11,5 +12,17 @@ public class StartActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_activity);
+
+        // Usage example
+        final String username = "";
+        final String password = "";
+        final AuthenticateUserTask task = new AuthenticateUserTask(this,
+                new AuthenticateUserTask.Parameters(username, password).setConnTimeout(5000)) {
+
+            @Override
+            public void onPostExecute(String accessToken, long refreshTime, long expiresIn) {
+                // TODO do something meaningful
+            }
+        };
     }
 }
