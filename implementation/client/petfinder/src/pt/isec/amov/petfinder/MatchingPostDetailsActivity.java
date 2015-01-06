811ab2f02401aafd1d8c9812bf21bb42b5a81e06
Marcos Góis <marcos.avbg@gmail.com>
2015-01-06 03:06:28 +0000

    Integrated the post fragment into the MatchingPostDetailsActivity

--- implementation/client/petfinder/src/pt/isec/amov/petfinder/MatchingPostDetailsActivity.java
+++ implementation/client/petfinder/src/pt/isec/amov/petfinder/MatchingPostDetailsActivity.java
@@ -1,15 +1,45 @@
 package pt.isec.amov.petfinder;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import pt.isec.amov.petfinder.core.PetFinderApp;
+import pt.isec.amov.petfinder.entities.Post;
+import pt.isec.amov.petfinder.rest.GetPostByIdTask;
 
 /**
- * Created by mgois on 30-12-2014.
+ *
  */
-public class MatchingPostDetailsActivity extends Activity {
+public class MatchingPostDetailsActivity extends Activity implements PostFragment.PostHost {
+
+    public static final String PARAM_POST_ID = "postId";
+
+    private Post post;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.matching_post_details_activity);
+
+        final FragmentManager fm = getFragmentManager();
+        final PetFinderApp app = (PetFinderApp) getApplication();
+        final int id = getIntent().getExtras().getInt(PARAM_POST_ID);
+
+        new GetPostByIdTask(app.getApiParams(), app.getToken().getAccessToken(), new GetPostByIdTask.Parameters(), id) {
+            @Override
+            public void onPostExecute(final Post post) {
+                // Save the post and introduce the fragment
+                MatchingPostDetailsActivity.this.post = post;
+
+                fm.beginTransaction()
+                        .add(R.id.myPost_frgPost, new PostFragment())
+                        .commit();
+            }
+        }.execute();
+    }
+
+    @Override
+    public Post getPost() {
+        return post;
     }
 }
